3e554862f4833a9b311c9ef03c0936be5c48a316
Ruthvick <ruthvick2005@gmail.com>
2023-01-15 10:17:23 -0500

    Final Version of Web API

--- main.py
+++ main.py
@@ -20,5 +20,24 @@ def api(station, date):
            "temperature": temp}
 
 
+@app.route("/api/v1/<station>")
+def station(station):
+    filename = 'test(jupyter)/data_small/TG_STAID' + station.zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    df['   TG'] = df['   TG']/10
+    result = df.to_dict(orient="record")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def year(station, year):
+    filename = 'test(jupyter)/data_small/TG_STAID' + station.zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['   TG'] = df['   TG'] / 10
+    df['    DATE'] = df['    DATE'].astype(str)
+    year_data = df[df['    DATE'].str.startswith(str(year))]
+    result = year_data.to_dict(orient='records')
+    return result
+
 if __name__ == '__main__':
     app.run(debug=True)
